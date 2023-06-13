111f032a2723b6ef0ae669a7850251ec4e40fc00
valexx55 <valexx55@gmail.com>
2023-06-13 13:45:51 +0200

    basicos

---
+++ basicosprofe/src/basicosprofe/EjercicosBasicos.java
@@ -0,0 +1,35 @@
+package basicosprofe;
+
+
+
+/**
+ * 
+ * LISTA DE EJERCICIOS DE REPASO A LA PARTE DE INICIACIÓN EN JAVA:
+ * 
+ * 1) HACER UN MÉTODO QUE RECIBA UNA CADENA Y UN CARACTER Y DIGA SI ESE CARACTER
+ * PERTENECE A LA CADENA O NO perteneceACadena 
+ * 2) HACER UN MÉTODO QUE RECIBA UNA
+ * CADENA Y UN CARACTER Y DIGA CÚANTAS VECES APARECE ESE CARACTER EN LA CADENA
+ * 2.1) HACER UN MÉTODO QUE RECIBA UNA CADENA Y UN CARACTER Y DIGA la última
+ * posición donde aparece ese caracter en esa cadena. Si no está, devuelve -1
+ * 
+ * "hola" a --> 3 "maja" a --> 3 "conejo" i --> -1
+ * 
+ * 
+ * 3) HACER UN MÉTODO QUE DADO UN NÚMERO, DIGA SI ES PAR O NO esPar
+ * 4) HACER UN MÉTODO QUE LE PIDA AL USUARIO SU EDAD Y LE DIGA SI ES MAYOR DE
+ * EDAD O NO mayorDeEdad 
+ * 5) HACER UN MÉTODO QUE RECIBA UNA NOTA DE 0 A 10 Y DIGA
+ * SI EQUIVALE A UN APROBADO, BIEN, NOTABLE, O SOBRESALIENTE clasificarNota -
+ * 6) HACER UN MÉTODO QUE RECIBA UNA CADENA Y LA DEVUELVA ALREVÉS invertirCadena 
+ * 7) HACER UN PROGRAMA QUE MUESTRE LA SECUENCIA 3, 6, 9, 12, 15 ...99 deTresEnTres
+ *
+ *
+ * // DEFINO UN MÉTODO // DARLE UN NOMBRE - camelCase // ID la entrada - // ID
+ * la salida -
+ *
+ * //pensar el los pasos en esapñol en psuedocódigo
+ */
+public class EjercicosBasicos {
+
+}
--- basicosprofe/src/basicosprofe/PruebaTest.java
+++ basicosprofe/src/basicosprofe/PruebaTest.java
@@ -1,5 +1,7 @@
 package basicosprofe;
 
+import java.io.File;
+
 public class PruebaTest {
 
 	public int calcula() {
@@ -10,10 +12,25 @@ public class PruebaTest {
 
 		System.out.println("HOLA MÁLAGA");
 
-		for (int a = 0; (a % 6) < 6; a++) {
-			System.out.println(a);
+//		for (int a = 0; (a % 6) < 6; a++) {
+//			System.out.println(a);
+//		}
+		
+		Object cadena = "HOLA";
+		//File fichero = (File)cadena;
+		
+		if (cadena instanceof String saludo)//pattern mathcing
+		{
+			saludo.length();
+			String palabra = (String)cadena;
+			int longi = palabra.length();
+			System.out.println("cadena es un String de tamaño " + longi);
+		} else {
+			System.out.println("cadena NO es un String");
 		}
 
+		Integer i = 66;
+		var bus = new File("f.txt");
 		// System.out.println (calcula());
 	}
 
