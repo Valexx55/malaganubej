af4d53ddbb885a2eb60e66fc36e74c1cd4c6b96d
valexx55 <valexx55@gmail.com>
2023-06-13 10:27:57 +0200

    v1

---
+++ basicosprofe/src/basicosprofe/PruebaTest.java
@@ -0,0 +1,20 @@
+package basicosprofe;
+
+public class PruebaTest {
+
+	public int calcula() {
+		return 1 + 1;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("HOLA MÁLAGA");
+
+		for (int a = 0; (a % 6) < 6; a++) {
+			System.out.println(a);
+		}
+
+		// System.out.println (calcula());
+	}
+
+}
